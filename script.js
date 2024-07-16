4978d79f18fef652f38d31297164e8aad6cd33e5
mr-crypter <omsaxena8426@gmail.com>
2024-07-16 16:25:02 +0530

    updated js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const doneButtons = document.querySelectorAll('.done-button');
+    const allPagesCheckbox = document.querySelector('.container input[type="checkbox"]');
+    const pageCheckboxes = document.querySelectorAll('.checkbox-container input[type="checkbox"]');
+
+    allPagesCheckbox.addEventListener('change', () => {
+        const isChecked = allPagesCheckbox.checked;
+        pageCheckboxes.forEach(checkbox => {
+            checkbox.checked = isChecked;
+        });
+    });
+
+    pageCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', () => {
+            const allChecked = Array.from(pageCheckboxes).every(checkbox => checkbox.checked);
+            allPagesCheckbox.checked = allChecked;
+        });
+    });
+
     doneButtons.forEach(button => {
         button.addEventListener('click', () => {
             alert('Done button clicked!');
